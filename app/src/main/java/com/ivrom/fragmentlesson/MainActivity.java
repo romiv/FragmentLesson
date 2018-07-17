cb7eb4c277fdaca74be491d8af8bf8f20dc34bc4
iv.rom96@gmail.com <MairenaDel19!>
2018-07-17 17:29:12 +0300

    Started implementation of dynamic fragment management (not finished)

--- app/src/main/java/com/ivrom/fragmentlesson/MainActivity.java
+++ app/src/main/java/com/ivrom/fragmentlesson/MainActivity.java
@@ -2,6 +2,7 @@ package com.ivrom.fragmentlesson;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +11,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements Fragment1.OnSelectedButtonListener {
-    TextView textView;
-    ImageView imageView;
+    public TextView textView;
+    public ImageView imageView;
+    private boolean mIsDynamic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,24 @@ public class MainActivity extends Activity implements Fragment1.OnSelectedButton
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textView);
         imageView = (ImageView) findViewById(R.id.imageView);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment2 fragment2 = (Fragment2) fragmentManager
+                .findFragmentById(R.id.fragment2);
+        mIsDynamic = fragment2 == null || !fragment2.isInLayout();
+        Toast.makeText(getApplicationContext(), mIsDynamic + "", Toast.LENGTH_SHORT).show();
+
+        // Зная, что второго фрагмента нет, загружаем первый
+        if (mIsDynamic) {
+            // начинаем транзакцию
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            // Создаем и добавляем первый фрагмент
+            Fragment1 fragment1 = new Fragment1();
+            ft.add(R.id.LinearLayout1, fragment1, "fragment1");
+            // Подтверждаем операцию
+            ft.commit();
+        }
+
     }
 
     @Override
@@ -28,6 +48,16 @@ public class MainActivity extends Activity implements Fragment1.OnSelectedButton
 
         // Получаем ссылку на второй фрагмент по ID
         Fragment2 fragment2 = (Fragment2) frManager.findFragmentById(R.id.fragment2);
+
+        if (mIsDynamic) {
+            // динамическое переключение на другой фрагмент
+        }
+        else {
+            // Если фрагмент недоступен
+            fragment2 = (Fragment2) frManager.findFragmentById(R.id.fragment2);
+            fragment2.setDescription(buttonIndex);
+        }
+
 
         if (fragment2 == null || !fragment2.isVisible()) {
             // Если фрагмента не существует, или он невидим
